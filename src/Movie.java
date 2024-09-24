a3b9e61428e54f536156d5a00177b6a88e02e1fc
BryanG2442 <bryangarcia2442@gmail.com>
2024-09-24 13:52:57 -0700

    Testing for class
    Hs

---
+++ src/Movie.java
@@ -0,0 +1,15 @@
+public class Movie {
+    static String userName = "Ba";
+    static String password = "b@2";
+    static String birthday = "12/2/31";
+    static boolean adOptIn = false;
+    static int age = 78;
+    public static void main(String[] args){
+        System.out.println("User name: " + userName);
+        System.out.println("Password: " + password);
+        System.out.println("Birthday :" + birthday);
+        System.out.println("Ad opt-in: " + adOptIn);
+        System.out.println("Age: " + age);
+
+    }
+}
